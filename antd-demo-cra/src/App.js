3f75c38ad9aab9434368a7d3b2549f70e4419969
opennext <opennext@126.com>
2018-04-24 10:30:53 +0800

    add antd example

---
+++ antd-demo-cra/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import Button from 'antd/lib/button';
+import { Button } from 'antd';
+import './App.css';
+
+class App extends Component {
+  public render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
